fe47537f824102e7f1a2968b0cbfbaf401c794fe
pedro <pedro@MacBook-Air-de-Pedro.local>
2024-04-10 23:07:07 -0400

    modulo 5 completo

---
+++ funciones/05-alcance.py
@@ -0,0 +1,12 @@
+def saludar():
+    saludo = 'Hola mundo'
+    return saludo
+
+
+def saludaChanchito():
+    saludo2 = 'Hola chanchito'
+    return saludo2
+
+
+print(saludar())
+print(saludaChanchito())
